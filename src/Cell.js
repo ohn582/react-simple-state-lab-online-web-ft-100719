2103f1bf1bafda3eb1843aeedb3b685771e720e1
Neal JIn Oh <no4259@gmail.com>
2020-02-23 03:24:39 +0000

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+  
+  constructor(){
+    super()
+    this.state={}
+  }
+
+  
+  render() {
+    return (
+      <div id="cell" style={{backgroundColor: '#fff'}}>
+      </div>
+    )
+  }
+  
+}
