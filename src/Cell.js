bd681d7f8f2f2d1d1f3cdbbd16afc4f21b9e3c79
Neal JIn Oh <no4259@gmail.com>
2020-03-13 23:44:32 +0000

    Automatically backed up by Learn

--- src/Cell.js
+++ src/Cell.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import Matrix from './Matrix' 
 
 export default class Cell extends Component {
   
   constructor(){
     super()
-    this.state={}
+    this.state={
+      color:''
+    }
   }
+  
+  
 
   
   render() {
     return (
-      <div id="cell" style={{backgroundColor: '#fff'}}>
+      <div id="cell" style={{backgroundColor: '#fff'}} className='cell' >
+        <button onClick={this.setState()}></button>
       </div>
     )
   }
